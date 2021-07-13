c1420ba55337c956528f3d704ba0cd31f1c2867f
David Miotti <david@station-coding.com>
2021-07-13 02:40:55 +0200

    Adds `Time.decimal_to_hms` (#3)
    * Adds decimal_to_hms
    
    * Limits the number of GH Actions.
    
    * Rename methods to be more idiomatic

--- bin/main.ml
+++ bin/main.ml
@@ -1,8 +1,12 @@
 open Lib
 
 let () =
-  let julian_date = Time.greenwich_to_julian_date 19.75 6 2009 in
-  Printf.printf "Julian date: %f\n" julian_date;
+  let julian_date = Time.julian_of_greenwich 19.75 6 2009 in
+  let day, month, year = Time.greenwich_of_julian julian_date in
+  Printf.printf "%f -> %f/%d/%d\n" julian_date day month year;
 
-  let day, month, year = Time.julian_to_greenwich_date julian_date in
-  Printf.printf "%f/%d/%d\n" day month year
+  let hours, minutes, seconds = Time.hms_of_decimal 18.52416667 in
+  Printf.printf "%d:%d:%d\n" hours minutes seconds;
+
+  let decimal = Time.decimal_of_hms 4. 40. 5.23 in
+  Printf.printf "Decimal: %f\n" decimal
--- lib/time.mli
+++ lib/time.mli
@@ -4,8 +4,14 @@ val string_of_month : int -> string
 val date_of_easter : int -> int * int
 (* [date_of_easter a] returns the date of Easter (day * month). *)
 
-val greenwich_to_julian_date : float -> int -> int -> float
-(* [greenwich_to_julian_date day month year] returns the Julian date. *)
+val julian_of_greenwich : float -> int -> int -> float
+(* [julian_of_greenwich day month year] returns the Julian date. *)
 
-val julian_to_greenwich_date : float -> float * int * int
-(* [julian_to_greenwich_date julian] converts a julian date into greenwich *)
+val greenwich_of_julian : float -> float * int * int
+(* [greenwich_of_julian julian] converts a julian date into greenwich. *)
+
+val hms_of_decimal : float -> int * int * int
+(* [hms_of_decimal hours] converts decimal hours to hours, minutes and seconds. *)
+
+val decimal_of_hms : float -> float -> float -> float
+(* [decimal_of_hms hours minutes seconds] converts hms to decimal hours. *) 
