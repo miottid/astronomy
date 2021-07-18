6bb9022a7501360ab25aa2c408e53bfe11400028
David Miotti <david@station-coding.com>
2021-07-18 20:39:01 +0200

    Update docs

--- src/timescale.mli
+++ src/timescale.mli
@@ -1,29 +1,32 @@
 (** Perform date and time calculations *)
 
 type date = { day: float; month: int; year: int }
-(** [date] represent a particular day with time included in [day]. *)
+(** [date] represent a particular day with time. *)
 
 type time = { hours: float; minutes: float; seconds: float }
-(** [time] is represent the time in a day hours, minutes and seconds. *)
+(** [time] is represent a specific time in hours, minutes and seconds. *)
 
 type datetime = { date: date; time: time }
 (** [datetime] is a date with time. *)
 
 type datetime_tz = { tzoffset: float; daylight: float; datetime: datetime }
-(** [datetime_tz] is a date with time, daylight saving and time zone. *)
+(** [datetime_tz] is a date with time, daylight saving and time zone offset.
+    [daylight] and [tzoffset] are expressed in hours. *)
 
 val pp_time : time -> string
-(** [pp_time time] converts [time] to a human readable string. *)
+(** [pp_time time] converts a [time] type to a human readable string.
+    e.g.: `2.5h 15.0m 25.0s`. *)
 
 val pp_date : date -> string
-(** [pp_date date] converts [date] to a human readable string. *)
+(** [pp_date date] converts [date] to a human readable string. 
+    e.g: `19.5/6/2021`. *)
 
 val string_of_month : int -> string
 (** [string_of_month a] returns the month name.
     Months starts from index 1. *)
 
 val string_of_weekday : int -> string
-(** [string_of_weekday weekday] returns the day of the week.
+(** [string_of_weekday weekday] returns the integer day of the week.
     0 is Sunday. *)
 
 val easter_day : int -> date
