54f60dd3310d00360e12821f33b0e0f1b0104350
David Miotti <david@station-coding.com>
2021-07-20 23:21:10 +0200

    Add tests for `Util.radians_of_degrees` and `Util.degrees_of_radians`

--- test/unit_util.ml
+++ test/unit_util.ml
@@ -9,5 +9,20 @@ let roundn () =
   Alcotest.(check @@ float @@ epsilon_float)
     "roundn" 3.14 (Util.roundn Float.pi 2)
 
+let radians_of_degrees () =
+  Alcotest.(check @@ float @@ epsilon_float)
+    "radians_of_degrees" 6.283185307179586
+    (Util.radians_of_degrees 360.)
+
+let degrees_of_radians () =
+  Alcotest.(check @@ float @@ epsilon_float)
+    "degrees_of_radians" 572.9577951308232
+    (Util.degrees_of_radians 10.)
+
 let test_set =
-  [ ("approx_equal", `Quick, approx_equal); ("roundn", `Quick, roundn) ]
+  [
+    ("approx_equal", `Quick, approx_equal);
+    ("roundn", `Quick, roundn);
+    ("radians_of_degrees", `Quick, radians_of_degrees);
+    ("degrees_of_radians", `Quick, degrees_of_radians);
+  ]
